7a55a9e7919677e3ad220ff64acce7cf24961684
Olaf <olaf.chuszno@gmail.com>
2024-10-11 11:12:41 +0200

    Style tiles and buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,10 @@ import { TransactionData } from '@/pages/api/transaction';
 import { getFixedFloatingPointNumber } from './helpers/getFixedFloatingPointNumber';
 import getTime from './helpers/getTime';
 import './page.scss';
+import parseTransactionTime from './helpers/parseTransactionTime';
 
 export interface TransactionDataToNextApi {
-  transaction_eur_amount: number
+  transaction_eur_amount: number;
 }
 
 interface TransactionError {
@@ -25,30 +26,42 @@ export default function Page() {
   const [inputAmountInEur, setInputAmountInEur] = useState<number | null>(null);
   const [inputAmountInEurError, setInputAmountInEurError] =
     useState<boolean>(false);
-  
-  const [transactionAmount, setTransactionAmount] = useState<number | null>(null)
-  const [forecastedTransactionPlnAmount, setForecastedTransactionPlnAmount] = useState<number | null>(null)
-  const [transactionDetails, setTransactionDetails] = useState<TransactionData | null>(null);
-  const [transactionError, setTransactionError] = useState<TransactionError | null>(null);
+
+  const [transactionAmount, setTransactionAmount] = useState<number | null>(
+    null
+  );
+  const [forecastedTransactionPlnAmount, setForecastedTransactionPlnAmount] =
+    useState<number | null>(null);
+  const [transactionDetails, setTransactionDetails] =
+    useState<TransactionData | null>(null);
+  const [transactionError, setTransactionError] =
+    useState<TransactionError | null>(null);
 
   const [isFinalisingTransaction, setIsFinalisingTransaction] = useState(false);
 
-  const [lastCurrencyRateUpdateTimestamp, setLastCurrencyRateUpdateTimestamp] = useState<string | null>(null);
+  const [lastCurrencyRateUpdateTimestamp, setLastCurrencyRateUpdateTimestamp] =
+    useState<string | null>(null);
 
-  const [allTransactions, setAllTransactions] = useState<null | TransactionData[]>(null)
-  const [isFetchingAllTransactions, setIsFetchingAllTransactions] = useState<boolean>(false);
-  const [fetchAllTransactionError, setFetchAllTransactionError] = useState<boolean>(false);
-  
+  const [allTransactions, setAllTransactions] = useState<
+    null | TransactionData[]
+  >(null);
+  const [isFetchingAllTransactions, setIsFetchingAllTransactions] =
+    useState<boolean>(false);
+  const [fetchAllTransactionError, setFetchAllTransactionError] =
+    useState<boolean>(false);
+
   useEffect(() => {
     console.log('--- transactionAmount: ---', transactionAmount);
     console.log('--- exchangeRate: ---', exchangeRate);
 
     if (transactionAmount && exchangeRate) {
-      setForecastedTransactionPlnAmount(getFixedFloatingPointNumber(transactionAmount * exchangeRate));
+      setForecastedTransactionPlnAmount(
+        getFixedFloatingPointNumber(transactionAmount * exchangeRate)
+      );
     } else {
       setForecastedTransactionPlnAmount(null);
     }
-  }, [exchangeRate, transactionAmount])
+  }, [exchangeRate, transactionAmount]);
 
   const getExchangeRate = () => {
     fetch('/api/exchange-rate')
@@ -84,22 +97,21 @@ export default function Page() {
   };
 
   const getAllTransactions = () => {
-    setIsFetchingAllTransactions(true)
+    setIsFetchingAllTransactions(true);
     setFetchAllTransactionError(false);
 
     fetch('/api/transaction')
       .then((response) => response.json())
-      .then((transactionsResponse: {transactions: TransactionData[]}) => {
-        setAllTransactions(transactionsResponse.transactions)
+      .then((transactionsResponse: { transactions: TransactionData[] }) => {
+        setAllTransactions(transactionsResponse.transactions);
       })
       .catch(() => {
-        setFetchAllTransactionError(true)
+        setFetchAllTransactionError(true);
       })
       .finally(() => {
-        setIsFetchingAllTransactions(false)
-    })
-  }
-
+        setIsFetchingAllTransactions(false);
+      });
+  };
 
   const finaliseTransaction = (transactionAmount: number) => {
     setTransactionDetails(null);
@@ -110,7 +122,9 @@ export default function Page() {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ transaction_eur_amount: transactionAmount } as TransactionDataToNextApi),
+      body: JSON.stringify({
+        transaction_eur_amount: transactionAmount,
+      } as TransactionDataToNextApi),
     })
       .then((response) => response.json())
       .then((transactionData: TransactionData) => {
@@ -119,11 +133,13 @@ export default function Page() {
       .catch(() => {
         const transactionError: TransactionError = {
           message: 'Could not finalise the transaction.',
-        }
+        };
 
-        setTransactionError(transactionError)
+        setTransactionError(transactionError);
       })
-      .finally(() => { setIsFinalisingTransaction(false) });
+      .finally(() => {
+        setIsFinalisingTransaction(false);
+      });
   };
 
   useEffect(() => {
@@ -134,25 +150,32 @@ export default function Page() {
     return () => clearInterval(exchangeFetchInterval);
   }, []);
 
-
   const isEurInputValid = useMemo(() => {
     return inputAmountInEur && inputAmountInEur >= 0;
   }, [inputAmountInEur]);
 
   return (
-    <main className='main'>
-      <section className='section-tile'>
-        <h2 className='title title--2'>Exchange rate</h2>
+    <main className="main">
+      <section className="section-tile">
+        <h2 className="title title--2">Exchange rate</h2>
 
-        <p>Exchange rate: <span data-cy="exchange-rate">{exchangeRate}</span></p>
+        <p>
+          Exchange rate: <span data-cy="exchange-rate">{exchangeRate}</span>
+        </p>
 
-        {lastCurrencyRateUpdateTimestamp && <p>Last exchange rate update: {lastCurrencyRateUpdateTimestamp}</p>}
+        {lastCurrencyRateUpdateTimestamp && (
+          <p>Last exchange rate update: {lastCurrencyRateUpdateTimestamp}</p>
+        )}
 
-        <button onClick={() => getExchangeRate()}>Update Exchange rate</button>
+        <div>
+          <button className='button' onClick={() => getExchangeRate()}>
+            Update Exchange rate
+          </button>
+        </div>
       </section>
 
-      <section className='section-tile'>
-        <h2 className='title title--2'>Conversion calculator</h2>
+      <section className="section-tile">
+        <h2 className="title title--2">Conversion calculator</h2>
 
         <form
           onSubmit={(event: React.FormEvent) => {
@@ -168,52 +191,53 @@ export default function Page() {
           }}
           action=""
         >
-          <input
-            onChange={(event) => {
-              event.preventDefault();
-              setInputAmountInEurError(false);
-
-              const valueFromInput: string = event.target.value;
-
-              console.log('valueFromInput:', valueFromInput);
-
-              const numericValueFromInput: number = +valueFromInput;
-
-              console.log('+event.target.value:', +event.target.value);
-
-              if (numericValueFromInput < 0) {
-                console.log('error in input');
-                setInputAmountInEurError(true);
-                return;
-              }
-
-              setInputAmountInEur(numericValueFromInput);
-            }}
-            value={inputAmountInEur || ''}
-            name="amount-in-euro"
-            placeholder="Euro amount"
-            type="number"
-          />
-
-          <button disabled={!isEurInputValid} type="submit">
-            Check amount in PLN
-          </button>
+          <div className='button-pair'>
+            <input
+              onChange={(event) => {
+                event.preventDefault();
+                setInputAmountInEurError(false);
+                const valueFromInput: string = event.target.value;
+                console.log('valueFromInput:', valueFromInput);
+                const numericValueFromInput: number = +valueFromInput;
+                console.log('+event.target.value:', +event.target.value);
+                if (numericValueFromInput < 0) {
+                  console.log('error in input');
+                  setInputAmountInEurError(true);
+                  return;
+                }
+                setInputAmountInEur(numericValueFromInput);
+              }}
+              value={inputAmountInEur || ''}
+              name="amount-in-euro"
+              placeholder="Euro amount"
+              type="number"
+              className='button-pair__child'
+            />
+            <button className='button button-pair__child' disabled={!isEurInputValid} type="submit">
+              Check amount in PLN
+            </button>
+            {!isEurInputValid && <p>Option is disabled until a correct amount is provided</p>}
+          </div>
 
           {inputAmountInEurError && <p>Please enter a valid number</p>}
         </form>
 
-        {/* TODO */}
         {currencyExchangeFormError && (
           <p>Error occurred when getting exchange rate. Please try again</p>
         )}
 
         {!currencyExchangeFormError && convertedCurrencyRate !== null && (
-          <div>Requested value in PLN: <span data-cy="converted-currency-rate">{convertedCurrencyRate}</span></div>
+          <div>
+            Requested value in PLN:{' '}
+            <span data-cy="converted-currency-rate">
+              {convertedCurrencyRate}
+            </span>
+          </div>
         )}
       </section>
 
-      <section className='section-tile'>
-        <h2 className='title title--2'>Make a Transaction</h2>
+      <section className="section-tile">
+        <h2 className="title title--2">Make a Transaction</h2>
 
         <form
           onSubmit={(event) => {
@@ -230,43 +254,46 @@ export default function Page() {
           }}
           action=""
         >
-          <input
-            onChange={(event) => {
-              event.preventDefault();
+          <div className='button-pair'>
+            <input
+              onChange={(event) => {
+                event.preventDefault();
+                const newTransactionInputValue = event.target.value;
+                if (newTransactionInputValue === '') {
+                  setTransactionAmount(null);
+                  return;
+                }
+                const newTransactionAmount = +event.target.value;
+                if (!newTransactionAmount || newTransactionAmount <= 0) {
+                  alert('Transaction amount has to be larger than 0');
+                  return;
+                }
+                setTransactionAmount(newTransactionAmount);
+              }}
+              value={transactionAmount || ''}
+              placeholder="Euro amount"
+              type="number"
+              className='button-pair__child'
+            />
 
-              const newTransactionInputValue = event.target.value;
-
-              if (newTransactionInputValue === '') {
-                setTransactionAmount(null);
-                return;
-              }
-
-              const newTransactionAmount = +event.target.value;
-
-              if (!newTransactionAmount || newTransactionAmount <= 0) {
-                alert('Transaction amount has to be larger than 0');
-                return;
-              }
-
-              setTransactionAmount(newTransactionAmount);
-            }}
-            value={transactionAmount || ''}
-            placeholder='Euro amount'
-            type="number"
-          />
-
-          <button type="submit">Make a transaction</button>
+            <button className='button button-pair__child' type="submit">Make a transaction</button>
+          </div>
         </form>
 
-        {forecastedTransactionPlnAmount && !transactionDetails && !isFinalisingTransaction && (
-          <div>
-            <p>Forecasted amonut in PLN: {forecastedTransactionPlnAmount}</p>
-            <p>(exchange rate is fluctuating and is likely to change)</p>
-        </div>)}
+        {forecastedTransactionPlnAmount &&
+          !transactionDetails &&
+          !isFinalisingTransaction && (
+            <div>
+              <p>Forecasted amonut in PLN: {forecastedTransactionPlnAmount}</p>
+              <p>(exchange rate is fluctuating and is likely to change)</p>
+            </div>
+          )}
 
         {isFinalisingTransaction && <p>Finalising the transaction...</p>}
 
-        {transactionError && <p>{transactionError.message}. Please try again.</p>}
+        {transactionError && (
+          <p>{transactionError.message}. Please try again.</p>
+        )}
 
         {transactionDetails && <p>Transaction completed!</p>}
 
@@ -285,24 +312,35 @@ export default function Page() {
                 <td>{transactionDetails.transaction_eur_amount}</td>
                 <td>{transactionDetails.transaction_pln_amount}</td>
                 <td>{transactionDetails.currenty_exchange_rate}</td>
-                <td>{transactionDetails.createdAt}</td>
+                <td>{parseTransactionTime(transactionDetails.createdAt)}</td>
               </tr>
             </tbody>
           </table>
         )}
       </section>
 
-      <section className='section-tile'>
-        <h2 className='title title--2'>All Transactions</h2>
-
-        {isFetchingAllTransactions && <p>Fetching transactions...</p>}
+      <section className="section-tile">
+        <h2 className="title title--2">All Transactions</h2>
 
         {fetchAllTransactionError && <p>Error fetching transactions.</p>}
 
-        {!isFetchingAllTransactions && !!allTransactions && !allTransactions.length && <p>No transactions yet.</p>}
+        {!isFetchingAllTransactions &&
+          !!allTransactions &&
+          !allTransactions.length && <p>No transactions yet.</p>}
 
-        <button onClick={() => {getAllTransactions()}}>Get all transactions</button>
-        {allTransactions?.length && <table>
+        <div>
+          <button className='button'
+            onClick={() => {
+              getAllTransactions();
+            }}
+          >
+            Get all transactions
+          </button>{' '}
+          {isFetchingAllTransactions && <span>Fetching transactions...</span>}
+        </div>
+
+        {allTransactions?.length && (
+          <table>
             <thead>
               <tr>
                 <th>Amount in EUR</th>
@@ -312,21 +350,19 @@ export default function Page() {
               </tr>
             </thead>
             <tbody>
-              {allTransactions.length && allTransactions.map((transaction: TransactionData) => (<tr key={transaction.id}>
-                <td>{transaction.transaction_eur_amount}</td>
-                <td>{transaction.transaction_pln_amount}</td>
-                <td>{transaction.currenty_exchange_rate}</td>
-                <td>{parseTransactionTime(transaction.createdAt)}</td>
-              </tr>))}
+              {allTransactions.length &&
+                allTransactions.map((transaction: TransactionData) => (
+                  <tr key={transaction.id}>
+                    <td>{transaction.transaction_eur_amount}</td>
+                    <td>{transaction.transaction_pln_amount}</td>
+                    <td>{transaction.currenty_exchange_rate}</td>
+                    <td>{parseTransactionTime(transaction.createdAt)}</td>
+                  </tr>
+                ))}
             </tbody>
-          </table>}
+          </table>
+        )}
       </section>
     </main>
   );
-}
-
-function parseTransactionTime(timestamp: string) {
-  const date = new Date(timestamp);
-
-  return `${date.getDate()}.${date.getMonth()}.${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
