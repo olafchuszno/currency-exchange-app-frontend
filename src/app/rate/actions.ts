c73dee5022003c793bd19748b1cb8fc1ecb6424a
Olaf <olaf.chuszno@gmail.com>
2024-10-18 14:12:39 +0200

    Remove redundant code

--- src/app/rate/actions.ts
+++ src/app/rate/actions.ts
@@ -5,9 +5,7 @@ import { revalidatePath } from "next/cache";
 export const handleRateRefresh = async () => {
   try {
     revalidatePath('/rate');
-  } catch (e) {
-    console.warn('Caught the error dummy', e)
-    
+  } catch {
     throw new Error('Failed to refresh the exchange rate. Please check your internet connection and try again.');
   }
 };
