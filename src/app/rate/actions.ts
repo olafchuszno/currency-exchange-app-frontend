0f737977ec7d874eb0ea8c629cb94ecbb2a0b336
Olaf <olaf.chuszno@gmail.com>
2024-10-18 14:11:16 +0200

    Handle rateRefresh errors

--- src/app/rate/actions.ts
+++ src/app/rate/actions.ts
@@ -3,5 +3,11 @@
 import { revalidatePath } from "next/cache";
 
 export const handleRateRefresh = async () => {
-  revalidatePath('/rate');
+  try {
+    revalidatePath('/rate');
+  } catch (e) {
+    console.warn('Caught the error dummy', e)
+    
+    throw new Error('Failed to refresh the exchange rate. Please check your internet connection and try again.');
+  }
 };
